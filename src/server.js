6579ffc547de7cc17921bfe0ccfaa906bba603ae
Fatinazimarcus <marcus_space@hotmail.com>
2024-05-15 20:15:57 -0300

    altera o server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log ("boas vindas a API do Allbooks")
+  console.log("API disponível Através da URL http://localhost:8000")
 })
